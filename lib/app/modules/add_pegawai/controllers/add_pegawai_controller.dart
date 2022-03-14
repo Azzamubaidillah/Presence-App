43b5332d0154c5e481d24730c2991fe1483b35a5
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-03-14 17:41:23 +0700

    done signup

--- lib/app/modules/add_pegawai/controllers/add_pegawai_controller.dart
+++ lib/app/modules/add_pegawai/controllers/add_pegawai_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
@@ -8,6 +9,7 @@ class AddPegawaiController extends GetxController {
   TextEditingController emailC = TextEditingController();
 
   FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   void addPegawai() async {
     if (nameC.text.isNotEmpty &&
@@ -17,6 +19,18 @@ class AddPegawaiController extends GetxController {
         UserCredential userCredential =
             await auth.createUserWithEmailAndPassword(
                 email: emailC.text, password: "password");
+
+        if (userCredential.user != null) {
+          String uid = userCredential.user!.uid;
+
+          await firestore.collection("pegawai").doc(uid).set({
+            "nip": nipC.text,
+            "name": nameC.text,
+            "email": emailC.text,
+            "uid": uid,
+            "createdAt": DateTime.now().toIso8601String(),
+          });
+        }
       } on FirebaseAuthException catch (e) {
         if (e.code == 'weak-password') {
           Get.snackbar(
